19aed0de00353cf32d33049c61a5e1375eb047a1
Blake Slyter <87281516+Bslyter@users.noreply.github.com>
2022-10-04 08:31:49 -0700

    Update setup_with_ddcv.R

--- setup_with_ddcv.R
+++ setup_with_ddcv.R
@@ -3,7 +3,6 @@ library(here)
 use_datadriven_cv(
   full_name = "Blake Slyter",
   data_location = "https://docs.google.com/spreadsheets/d/14MQICF2F8-vf8CKPF1m4lyGKO6_thG-4aSwat1e2TWc",
-  pdf_location = "https://github.com/bslyter/cv/raw/master/strayer_cv.pdf",
   html_location = "bslyter.me/cv/",
   source_location = "https://github.com/bslyter/cv",
   output_dir = here(),
